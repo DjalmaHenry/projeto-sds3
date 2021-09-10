3957e57e13bdd31621beef5549f997df1345b701
Djalma Henrique Silva Lima <45500812+DjalmaHenry@users.noreply.github.com>
2021-09-10 10:25:12 -0300

    add project

---
+++ backend/src/main/java/com/devsuperior/djvendas/DjvendasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.djvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjvendasApplication.class, args);
+	}
+
+}
